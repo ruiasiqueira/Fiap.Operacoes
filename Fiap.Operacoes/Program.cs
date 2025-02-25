7d1f02a43e66b5bd399970652c39982352d8d07c
ruiasiqueira <rm98436@fiap.com.br>
2025-02-25 20:29:50 -0300

    verificação

--- Fiap.Operacoes/Program.cs
+++ Fiap.Operacoes/Program.cs
@@ -39,6 +39,19 @@ namespace Fiap.Operacoes
             Console.WriteLine($"Os números são diferentes?{num1 != num2}");
             Console.WriteLine($"O primeiro número é maior?{num1 > num2}");
             Console.WriteLine($"O primeiro número é menor?{num1 < num2}");
+
+            Console.WriteLine("\n Verificação Lógica:");
+
+            bool ambosPositivos = num1>0 || num2>0;
+            bool NegativoPositivo = num1<0 || num2<0;     
+            bool nenhumNegativo = !(num1<0 || num2<0);
+
+            Console.WriteLine($"Ambos positivos {ambosPositivos}");
+            Console.WriteLine($"Ao menos um negativo {NegativoPositivo}");
+            Console.WriteLine($"Nenhum número negativo {nenhumNegativo}");
+
+            Console.WriteLine("\n Pressione qualquer tecla para sair");
+            
             Console.ReadKey();
 
         }
