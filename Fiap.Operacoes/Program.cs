9c709e35283d9b5f0d2ba6de9720272f8c3d4688
labsfiap <labsfiap@PA307MICRO30>
2025-02-25 20:08:28 -0300

    Add project files.

---
+++ Fiap.Operacoes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Operacoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome");
+
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite um número");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite outro número");
+
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double soma = num1 + num2;
+            double sub = num1 - num2;
+            double mult = num1 * num2;
+            double modulo = num1 % num2;
+
+            Console.WriteLine("\n Resultados das Op Aritm");
+            Console.WriteLine($"Soma: {soma}");
+            Console.WriteLine($"Sub: {sub}");
+            Console.WriteLine($"Multi: {mult}");
+            Console.WriteLine($"Modulo: {modulo}");
+
+            Console.ReadKey();
+
+        }
+            
+    }
+}
